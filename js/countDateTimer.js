e48e5082fe0885a8fa6a727a01a8d4aaa7d83b79
Николай Гребенюк <Yanukhous@ukr.net>
2021-08-09 15:03:41 +0300

    НАписано скрипт для 1 та 3 задач

--- js/countDateTimer.js
+++ js/countDateTimer.js
@@ -1,9 +1,5 @@
-// Создай плагин настраиваемого таймера, который ведет обратный отсчет до предварительно определенной даты.
-// Такой плагин может использоваться в блогах и интернет-магазинах, страницах регистрации событий,
-// во время технического обслуживания и т. д.
-
 const refs = {
-    stopTimer: document.querySelector('timer'),
+  stopTimer: document.querySelector('.timer'),
   divTimer: document.querySelector('#timer-1'),
   spanDays: document.querySelector('[data-value="days"]'),
   spanHours: document.querySelector('[data-value="hours"]'),
@@ -11,62 +7,52 @@ const refs = {
   spanSecs: document.querySelector('[data-value="secs"]'),
 };
 
-const timer = {
-// constructor({ CountdownTimer }) {
-//     this.CountdownTimer = CountdownTimer;
-// }
-
-  start() {
-    const startTime = Date.now('Ogs 4, 2021');
-
-    setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-      const time = setTimeComponents(deltaTime);
-      
-    //   updateClock(time)
-      
-    }, 1000);
-  },
-};
-
-timer.start();
-
-  function updateClock({days, hours, mins, secs}) {
-       refs.divTimer.textContent = `${days}:${hours}:${mins}:${secs}`;
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
   }
 
-  function pad(value) {
-      return String(value).padStart(2, '0')
+  setInt = setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = this.targetDate - currentTime;
+    this.setTimeComponents(deltaTime);
+    this.stopTime(deltaTime);
+  }, 1000);
+
+  pad(value) {
+    return String(value).padStart(2, '0');
   }
 
+  setTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-function setTimeComponents(time) {
-const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    refs.spanDays.textContent = `${days}`;
+    refs.spanHours.textContent = `${hours}`;
+    refs.spanMins.textContent = `${mins}`;
+    refs.spanSecs.textContent = `${secs}`;
+  }
 
-refs.spanDays.textContent = `${days}`;
-refs.spanHours.textContent = `${hours}`;
-refs.spanMins.textContent = `${mins}`;
-refs.spanSecs.textContent = `${secs}`;
-}
-
-
-
-function stopTime(time) {
-    if(time < 0) {
-        refs.stopTimer.textContent = 'Поздравляю, вы выиграли автомобиль';
-        refs.stopTimer.style.backgroundColor = 'rgba(66, 60, 60, 0.5)';
-        refs.stopTimer.style.fontSize = '48px';
-        refs.stopTimer.style.color = 'white';
-        refs.stopTimer.style.textAlign = 'center';
-        refs.stopTimer.style.fontWeight = '700'
+  stopTime(time) {
+    if (time < 0) {
+      refs.stopTimer.textContent = 'You missed the action. ))) We are waiting for you next time. Best wishes.';
+      refs.stopTimer.style.backgroundColor = 'rgba(66, 60, 60, 0.5)';
+      refs.stopTimer.style.fontSize = '48px';
+      refs.stopTimer.style.color = 'red';
+      refs.stopTimer.style.textAlign = 'center';
+      refs.stopTimer.style.fontWeight = '700';
+      refs.stopTimer.style.width = '700px';
+      refs.stopTimer.style.padding = '25px';
     }
+  }
 }
 
-// new CountdownTimer({
-//     selector: '#timer-1',
-//     targetDate: new Date('Jul 17, 2019'),
-//   });
+const Countdown = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Aug 10, 2021'),
+});
